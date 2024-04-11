2e1166c7125b554217727fb4c7f06a0366a39c7b
harold armijo leon <harmijoleon@gmail.com>
2024-04-11 12:05:03 +0200

    update path 2

--- tmux-layouts/nix_config-term.window.sh
+++ tmux-layouts/nix_config-term.window.sh
@@ -1,6 +1,6 @@
 # Set window root path. Default is `$session_root`.
 # Must be called before `new_window`.
-window_root "~/code/seven/nix_config-term"
+window_root "~/code/seven/nix-config"
 
 # Create new window. If no argument is given, window name will be based on
 # layout file name.

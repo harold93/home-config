74bb11b06dd6c6815834c32eafa92843d64f35e5
Harold ARMIJO LEON <harmijoleon@gmail.com>
2024-04-12 11:54:42 +0200

    harpoon

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -181,6 +181,39 @@ return {
   },
   -- highlight var under cursor
   {"RRethy/vim-illuminate"},
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function ()
+      local harpoon = require("harpoon")
+
+      -- REQUIRED
+      harpoon:setup()
+      -- REQUIRED
+
+      vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end)
+      vim.keymap.set("n", "<leader>hl", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+      vim.keymap.set("n", "<A-1>", function() harpoon:list():select(1) end)
+      vim.keymap.set("n", "<A-2>", function() harpoon:list():select(2) end)
+      vim.keymap.set("n", "<A-3>", function() harpoon:list():select(3) end)
+      vim.keymap.set("n", "<A-4>", function() harpoon:list():select(4) end)
+
+      -- Toggle previous & next buffers stored within Harpoon list
+      vim.keymap.set("n", "<A-h>", function() harpoon:list():prev() end)
+      vim.keymap.set("n", "<A-l>", function() harpoon:list():next() end)
+
+      -- mac compa
+      vim.keymap.set("n", "¡", function() harpoon:list():select(1) end)
+      vim.keymap.set("n", "™", function() harpoon:list():select(2) end)
+      vim.keymap.set("n", "£", function() harpoon:list():select(3) end)
+      vim.keymap.set("n", "¢", function() harpoon:list():select(4) end)
+
+      vim.keymap.set("n", "˙", function() harpoon:list():prev() end)
+      vim.keymap.set("n", "¬", function() harpoon:list():next() end)
+    end
+  },
   -- Text and Edition end
   -- ****************************************
 

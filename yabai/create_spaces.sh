eca494e5dd9bdcea83b774bc9b673a3a19835e3a
Harold ARMIJO LEON <harmijoleon@gmail.com>
2024-11-19 02:41:28 +0100

    testing conf yabai  and skhd

---
+++ yabai/create_spaces.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+function setup_space {
+  local idx="$1"
+  local name="$2"
+  local space=
+  echo "setup space $idx : $name"
+
+  space=$(yabai -m query --spaces --space "$idx")
+  if [ -z "$space" ]; then
+    yabai -m space --create
+  fi
+
+  yabai -m space "$idx" --label "$name"
+}
+
+
+setup_space 1 web
+setup_space 2 notion
+setup_space 3 figma
+setup_space 4 messaging
+setup_space 5 music
+setup_space 6 code_rubymine
+setup_space 7 code_android_studio
+setup_space 8 terminal
+
+# Move space 6/7/8 to the second display when it's connected
+yabai -m space 6 --display 2
+yabai -m space 7 --display 2
+yabai -m space 8 --display 2
+# Assign space 6/7/8 to the second display
+yabai -m display --add 2 --space 6
+yabai -m display --add 2 --space 7
+yabai -m display --add 2 --space 8

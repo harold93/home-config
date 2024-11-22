02e5606a99e09e20ed8fb91bb3c7aa0b048c6b1a
Harold ARMIJO LEON <harmijoleon@gmail.com>
2024-11-22 16:24:51 +0100

    testing  new sapces

--- yabai/create_spaces.sh
+++ yabai/create_spaces.sh
@@ -20,18 +20,19 @@ setup_space 2 notion
 setup_space 3 figma
 setup_space 4 messaging
 setup_space 5 music
-setup_space 6 code_rubymine
-setup_space 7 code_android_studio
-setup_space 8 terminal
-setup_space 9 tmp_space
+setup_space 6 tmp_space_first
+setup_space 7 terminal
+setup_space 8 code_rubymine
+setup_space 9 code_android_studio
+setup_space 0 tmp_space_second
 
 # Move spaces to the second display when it's connected
-yabai -m space 6 --display 2
 yabai -m space 7 --display 2
 yabai -m space 8 --display 2
 yabai -m space 9 --display 2
+yabai -m space 0 --display 2
 # Assign spaces to the second display
-yabai -m display --add 2 --space 6
 yabai -m display --add 2 --space 7
 yabai -m display --add 2 --space 8
 yabai -m display --add 2 --space 9
+yabai -m display --add 2 --space 0

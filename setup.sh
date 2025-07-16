0bb2225c82f18410f5b83faec604e295094cac39
Harold <harmijoleon@gmail.com>
2025-07-16 07:49:28 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,12 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+cd ~/
+mkdir -p code/pers
+cd code/pers
+git clone https://github.com/harold93/home-config.git
+cd home-config
+
 brew bundle
 
 stow .

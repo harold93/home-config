584ab9c57443df05075e351050c2ea16889787bf
Harold <harmijoleon@gmail.com>
2025-08-28 00:16:47 +0200

    chore: dark mode nvim

--- src/dot-config/nvim/lua/plugins.lua
+++ src/dot-config/nvim/lua/plugins.lua
@@ -1032,4 +1032,42 @@ return {
 	},
 	-- Git integration end
 	-- ****************************************
+	{ 
+    "catppuccin/nvim", 
+    name = "catppuccin", 
+    priority = 1000,
+    opts = {
+      transparent_background = true
+    }
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      transparent = true
+    },
+  },
+	{
+		"cormacrelf/dark-notify",
+		config = function()
+			local dn = require("dark_notify")
+
+			-- mac bin notify
+			-- brew install cormacrelf/tap/dark-notify
+			-- Configure
+			dn.run({
+				schemes = {
+					dark = "tokyonight-moon",
+					light = "catppuccin-latte",
+				},
+				onchange = function(mode)
+					-- optional, you can configure your own things to react to changes.
+					-- this is called at startup and every time dark mode is switched,
+					-- either via the OS, or because you manually set/toggled the mode.
+					-- mode is either "light" or "dark"
+				end,
+			})
+		end,
+	},
 }
